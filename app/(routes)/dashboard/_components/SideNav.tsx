c4e268431c5d3dd5e52533a484d5fc43481dd564
DishaSingh07 <dishumau@gmail.com>
2025-01-23 09:52:57 +0530

    sidenav bottom ready

--- app/(routes)/dashboard/_components/SideNav.tsx
+++ app/(routes)/dashboard/_components/SideNav.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Image from 'next/image'
-import { ChevronDown } from 'lucide-react'
+import { Archive, ChevronDown, Flag, Github } from 'lucide-react'
 import SideNavTopSection from './SideNavTopSection'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import SideNavBottomSection from './SideNavBottomSection'
 
 
 function SideNav() {
 
     const { user }: any = useKindeBrowserClient();
+  
 
     return (
-        <div className='h-screen fixed w-72 border-r p-6 border-[1px]'>
-            <SideNavTopSection user={user} />
+        <div className='h-screen fixed w-72 border-r p-6 border-[1px]
+        flex flex-col'>
+            <div className='flex-1'>
+                <SideNavTopSection user={user} />
+            </div>
+
+            <div>
+                <SideNavBottomSection />
+            </div>
         </div>
     )
 }
