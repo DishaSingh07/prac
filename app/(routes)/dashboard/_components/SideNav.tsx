f23948f40b446eefe5347234cfc17cfa4f0e2d47
DishaSingh07 <dishumau@gmail.com>
2025-01-23 09:24:02 +0530

    SideNav top

---
+++ app/(routes)/dashboard/_components/SideNav.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import { ChevronDown } from 'lucide-react'
+import SideNavTopSection from './SideNavTopSection'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+
+
+function SideNav() {
+
+    const {user}:any = useKindeBrowserClient();
+
+    return (
+        <div className='bg-gray-100 h-screen fixed w-72 border-r p-6'>
+            <SideNavTopSection user={user}/>
+        </div>
+    )
+}
+
+export default SideNav
